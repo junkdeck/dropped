0434858525869ded14b43b269ffd4c47b536607d
Frederik Bak <fred@mslsr.com>
2019-10-12 15:14:41 +0200

    render list in Main

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,22 +3,43 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import Input from 'src/components/Input'
+import ListItem from 'src/components/ListItem'
 
 import {fetchManagers} from 'src/state/data/creators'
+import {getFormattedEmployeeList} from 'src/state/data/selectors'
 
 class Main extends Component {
+  state = {
+    input: '',
+  }
+
+  onChange = e => {
+    this.setState({input: e.target.value})
+  }
+
   componentDidMount() {
     this.props.fetchManagers()
   }
 
   render() {
+    console.log(this.state.input)
     return (
       <div>
-        <Input />
+        <Input value={this.state.input} onChange={this.onChange} />
+
+        {this.props.list
+          .filter(x => x.name.includes(this.state.input))
+          .map(x => (
+            <ListItem user={x} key={x.name} />
+          ))}
       </div>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  list: getFormattedEmployeeList(state),
+})
 
 const mapDispatchToProps = dispatch => ({
   ...bindActionCreators(
@@ -30,6 +51,6 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(Main)

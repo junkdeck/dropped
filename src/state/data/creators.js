3686c15b4c712092e14752e463b031814c64b86e
Frederik Bak <fred@mslsr.com>
2019-10-12 14:26:57 +0200

    fetch list from list url

--- src/state/data/creators.js
+++ src/state/data/creators.js
@@ -1,6 +1,21 @@
+import axios from 'axios'
+
 import {LOAD_REQUEST, LOAD_ERROR, LOAD_SUCCESS} from './actions'
+
+import {LIST_URL} from 'src/constants'
 
 export const fetchManagers = () => dispatch => {
   console.log('fetch manager list')
   dispatch({type: LOAD_REQUEST})
+
+  axios
+    .get(LIST_URL)
+    .then(res => {
+      console.log(res)
+      dispatch({type: LOAD_SUCCESS, list: res.data.data})
+    })
+    .catch(err => {
+      console.log(Object.entries(err))
+      dispatch({type: LOAD_ERROR})
+    })
 }

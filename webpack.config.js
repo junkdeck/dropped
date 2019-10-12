de2d5d8486fa05e433bfd1e95157d9c0eb9c8603
Frederik Bak <fred@mslsr.com>
2019-10-12 13:10:11 +0200

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'dist/bundle.js',
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    stats: {
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+    },
+  },
+  resolve: {
+    modules: [path.resolve('./src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+}

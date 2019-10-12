fa3e024ee192422c6f296d10f00deb627ea25c26
Frederik Bak <fred@mslsr.com>
2019-10-12 14:12:42 +0200

    add Main view

---
+++ src/views/Main.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
+import Input from 'src/components/Input'
+
+import {fetchManagers} from 'src/state/data/creators'
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Input />
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators(
+    {
+      fetchManagers,
+    },
+    dispatch,
+  ),
+})
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Main)
